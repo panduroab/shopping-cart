43041af79f45a0ae8edbf624aa95900aa512247a
GerardoGuti <developer@MA-E15-1009.local>
2018-01-16 16:07:37 -0600

    Client Model ready

---
+++ src/db/models/client.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const collectionName = 'client';
+
+var clientSchema = new Schema({
+    name :  String,
+    lastnamefa : String,
+    lastnamemo : String,
+    birthdate : Date,
+    address : String
+});
+
+var  clientModel = mongoose.model(collectionName,schema);
+module.exports = clientSchema;
