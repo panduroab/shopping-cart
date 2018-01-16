fec78a95696e29e1eb117c573f8e8d3dcb4dccf4
Abraham Panduro <panduroab@gmail.com>
2018-01-16 13:07:09 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const app = require('./src/server')();
+const port = 3000;
+
+app.listen(port, () => console.log('Server is running at:', port));
---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+
+module.exports = () => {
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    app.get('/', (req, res, next) => {
+        res.send(`Shopping Cart API`)
+    });
+
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+        let err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+
+    // error handler
+    app.use(function (err, req, res, next) {
+        let status = err.status || 500;
+        res.status(status);
+        res.send({
+            message: err.message,
+            status
+        });
+    });
+
+    return app;
+};
