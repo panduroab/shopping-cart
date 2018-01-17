fca7216206527d4f07b6dc884f1334df6086cad5
GerardoGuti <developer@MA-E15-1009.local>
2018-01-17 10:23:06 -0600

    Merge

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -2,3 +2,14 @@ const route  = require('express').Router();
 const clientOrder = require('../db/models/client');
 
 //GET
+route.get('/',(req,res)=>{
+    res.status(200)
+})
+//Post
+route.post('/',(req,res)=>{
+    res.status(200)
+})
+//Delete
+route.delete('/:id',(req, res)=>{
+    res.status(200)
+})
