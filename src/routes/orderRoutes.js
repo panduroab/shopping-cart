f5b79bfa464bf8d582e2bd76275b31e23ad6cb1d
JFrankLL <flopez@itexico.net>
2018-01-17 08:14:37 -0600

    routes file for order model

---
+++ src/routes/orderRoutes.js
@@ -0,0 +1,25 @@
+const   route   = require('express').Router(),
+    OrderModel  = require('../db/models/order');
+
+// #region GET ------------------------------------------------------------------------------------
+route.get('/', (req, res) => {
+    
+});
+// #endregion
+// #region POST -----------------------------------------------------------------------------------
+route.post('/', (req, res) => {
+    
+});
+// #endregion
+// #region PUT ------------------------------------------------------------------------------------
+route.put('/:id', (req, res) => {
+    
+});
+// #endregion
+// #region DELETE ---------------------------------------------------------------------------------
+route.delete('/:id', (req, res) => {
+    
+});
+// #endregion
+
+module.exports = route;
--- src/server.js
+++ src/server.js
@@ -13,6 +13,8 @@ module.exports = () => {
         res.send(`Shopping Cart API`);
     });
 
+    
+
     // catch 404 and forward to error handler
     app.use(function (req, res, next) {
         let err = new Error('Not Found');
