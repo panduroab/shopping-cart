c9274bd256abe525bc13cee7d7f4f35f93e72823
ssmarin <ssmarin@itexico.com>
2018-01-18 12:49:04 -0600

    add get id order

--- __tests__/order/orderTest.expect.js
+++ __tests__/order/orderTest.expect.js
@@ -4,10 +4,10 @@ const chai = require('chai');
 const server = require('../../src/server')({ logger: false });
 const db = require('../../src/db/db');
 
-describe('API Order', function() {
-    db({ domain: '127.0.0.1', port: '27017', dbNme: 'shopping-cart' });
-    server.listen(3000, () => console.log('Server is running at:', 3000));
-    it('GET (all) order', function(done) {
+describe('API Order', function () {
+    // db({ domain: '127.0.0.1', port: '27017', dbNme: 'shopping-cart' });
+    // server.listen(3000, () => console.log('Server is running at:', 3000));
+    it('GET (all) order', function (done) {
         // this.timeout(100);
         api.get('/api/order')
             .set('Accept', 'application/json')
@@ -15,6 +15,20 @@ describe('API Order', function() {
             .expect(200)
             .then(res => {
                 //console.log(res.body);
+                done();
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+    it('Should get order /id', done => {
+        let url = '/api/order';
+        let id = '5a5f7aa660373a0c27edf708';
+        api.get(`${url}/${id}`)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(res => {
                 done();
             })
             .catch(err => {
