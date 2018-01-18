70cb33354c97a2425e3ca65ce21173bf874510fc
ssmarin <ssmarin@itexico.com>
2018-01-18 13:12:31 -0600

    add delete order comment

--- __tests__/order/orderTest.expect.js
+++ __tests__/order/orderTest.expect.js
@@ -5,8 +5,8 @@ const server = require('../../src/server')({ logger: false });
 const db = require('../../src/db/db');
 
 describe('API Order', function () {
-    // db({ domain: '127.0.0.1', port: '27017', dbNme: 'shopping-cart' });
-    // server.listen(3000, () => console.log('Server is running at:', 3000));
+    db({ domain: '127.0.0.1', port: '27017', dbNme: 'shopping-cart' });
+    server.listen(3000, () => console.log('Server is running at:', 3000));
     it('GET should fetch all orders', done => {
         api.get('/api/order')
             .set('Accept', 'application/json')
@@ -22,7 +22,7 @@ describe('API Order', function () {
     });
     it('Should get order /id', done => {
         let url = '/api/order';
-        let id = '5a5f7aa660373a0c27edf708';
+        let id = '5a60ecc523ac9505ff84dd2d';
         api.get(`${url}/${id}`)
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
@@ -43,4 +43,18 @@ describe('API Order', function () {
             .then(res => done())
             .catch(err => done(err));
     });
+    // it('Should delete order /id', done => {
+    //     let url = '/api/order';
+    //     let id = '5a5f7aa660373a0c27edf708';
+    //     api.delete(`${url}/${id}`)
+    //         .set('Accept', 'application/json')
+    //         .expect('Content-Type', /json/)
+    //         .expect(200)
+    //         .then(res => {
+    //             done();
+    //         })
+    //         .catch(err => {
+    //             console.log(err);
+    //         });
+    // });
 });
