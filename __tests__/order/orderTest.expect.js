1c8025fbffec897c564e4925af343bd9a3a3c35c
ssmarin <ssmarin@itexico.com>
2018-01-19 15:30:58 -0600

    Add license

--- __tests__/order/orderTest.expect.js
+++ __tests__/order/orderTest.expect.js
@@ -50,7 +50,7 @@ describe('API Order', function () {
             let id = order._id;
             supertest(server).put(`/api/order/${id}`)
                 .set('Accept', 'application/json')
-                .expect('Content-Type', /json/)
+                .expect('Content-Type', /text/)
                 .send(orderObj)
                 .expect(200)
                 .then(res => done())
@@ -63,7 +63,7 @@ describe('API Order', function () {
             let order = result;
             let id = order._id;
             supertest(server).delete(`${url}/${id}`)
-                // .set('Accept', 'application/json')
+                .set('Accept', 'application/json')
                 .expect('Content-Type', /text/)
                 .expect(200)
                 .then(res => done())
---
+++ readme.md
@@ -0,0 +1,26 @@
+* SHOPPING-CAR
+
+* Getting Started
+
+* Prerequisites
+
+Project have dependencies that require:
+node v8.9.4, mongo v3.6.1, together with NPM 5.6.0
+
+* Installing
+
+npm install
+
+* Running the tests
+
+Project require running mongod
+npm test
+
+* Authors
+
+GerardoGuti
+JFrankLL
+MarioGutierrezM
+ssmarin
+
+* License
