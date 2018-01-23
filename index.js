90b2134a5c340e711d28670c774cc569d5009d17
JFrankLL <flopez@itexico.net>
2018-01-23 08:23:23 -0600

    databse connection changed in all instances

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const db = require('./src/db/db');
 const port = 3000;
+const app = require('./src/server');
 
-const config = {
-    logger: 'dev',
-    dbConfig: {
-        domain: '127.0.0.1',
-        port: '27017',
-        dbName: 'shopping-cart'
-    }
-};
+// Database Connection
+db({ domain: '127.0.0.1', port: '27017', dbName: 'shopping-cart' })
+    .then(con => console.log('Connected to MongoDB succesfully!!'))
+    .catch(err => console.log(err));
 
-const app = require('./src/server')(config);
-
-app.listen(port, () => console.log('Server is running at:', port));
+// App
+app({ logger: 'dev' }).listen(port, () => console.log('Server is running at:', port));
