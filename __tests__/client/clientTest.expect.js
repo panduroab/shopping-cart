50015902de8b1c6fee9ec4d311c6a04aca2e0581
ssmarin <ssmarin@itexico.net>
2018-01-23 11:30:30 -0600

    refactoring code

--- __tests__/client/clientTest.expect.js
+++ __tests__/client/clientTest.expect.js
@@ -7,109 +7,109 @@ const clientModel = require('../../src/db/models/client');
 const server = require('../../src/server')({ logger: false });
 const db = require('../../src/db/db')({ domain: '127.0.0.1', port: '27017', dbName: 'shopping-cart' }).then(con => { }).catch(err => { });
 
-// describe('ClientController', () => {
-//     it('Should create a client', done => {
-//         let clientObj = {
-//             name: 'name',
-//             lastnamefa: 'lastnamefa',
-//             lastnamemo: 'lastnamemo',
-//             birthdate: '1985-01-23',
-//             address: 'address #281'
-//         };
-//         clientController.postClient(clientObj)
-//             .then(clients => done())
-//             .catch(err => done(err));
-//     });
-//     it('Should fetch all clients', done => {
-//         clientController.getAllClients()
-//             .then(clients => done()).catch(err => done(err));
-//     });
-//     it('Should fetch a client', done => {
-//         clientController.getRandomClient().then(client => {
-//             let id = client._id;
-//             clientController.getClient(id)
-//                 .then(client => {
-//                     client.should.to.have.property('name');
-//                     client.name.should.to.not.equal(null);
-//                     done();
-//                 }).catch(err => {
-//                     done(err);
-//                 });
-//         }).catch();
-//     });
-//     it('Should delete a client', done => {
-//         clientController.getRandomClient().then(client => {
-//             let id = client._id;
-//             clientController.deleteClient(id)
-//                 .then(result => {
-//                     result.should.be.a('string');
-//                     done();
-//                 }).catch(err => {
-//                     done(err);
-//                 });
-//         }).catch();
-//     });
-// });
+describe('ClientController', () => {
+    it('Should create a client', done => {
+        let clientObj = {
+            name: 'name',
+            lastnamefa: 'lastnamefa',
+            lastnamemo: 'lastnamemo',
+            birthdate: '1985-01-23',
+            address: 'address #281'
+        };
+        clientController.postClient(clientObj)
+            .then(clients => done())
+            .catch(err => done(err));
+    });
+    it('Should fetch all clients', done => {
+        clientController.getAllClients()
+            .then(clients => done()).catch(err => done(err));
+    });
+    it('Should fetch a client', done => {
+        clientController.getRandomClient().then(client => {
+            let id = client._id;
+            clientController.getClient(id)
+                .then(client => {
+                    client.should.to.have.property('name');
+                    client.name.should.to.not.equal(null);
+                    done();
+                }).catch(err => {
+                    done(err);
+                });
+        }).catch();
+    });
+    it('Should delete a client', done => {
+        clientController.getRandomClient().then(client => {
+            let id = client._id;
+            clientController.deleteClient(id)
+                .then(result => {
+                    result.should.be.a('string');
+                    done();
+                }).catch(err => {
+                    done(err);
+                });
+        }).catch();
+    });
+});
 
-// describe('API Client', function () {
-//     it('POST should create an client', done => {
-//         let client = {
-//             name: 'Gerardo',
-//             lastnamefa: 'Gutierrez',
-//             lastnamemo: 'Castaneda',
-//             birthdate: new Date(1993, 11, 20),
-//             address: 'Somewhere #12'
-//         };
-//         supertest(server).post('/api/client')
-//             .set('Accept', 'application/json')
-//             .send(client)
-//             .expect(200)
-//             .then(res => done())
-//             .catch(err => done(err));
-//     });
-//     it('Get should fetch all clients', done => {
-//         supertest(server).get('/api/client')
-//             .set('Accept', 'application/json')
-//             .expect('Content-Type', /json/)
-//             .expect(200)
-//             .then(res => {
-//                 id = res.body._id;
-//                 done();
-//             })
-//             .catch(err => { done(err); });
-//     });
-//     it('Should get client /id', done => {
-//         clientController.getRandomClient().then(client => {
-//             let id = client._id;
-//             supertest(server).get(`/api/client/${id}`)
-//                 .set('Accept', 'application/json')
-//                 .expect('Content-Type', /json/)
-//                 .expect(200)
-//                 .end((err, res) => {
-//                     if (err)
-//                         return done(err);
-//                     done();
-//                 });
-//         }).catch(err => {
-//             done(err);
-//         });
-//     });
-//     it('Should delete client /id', done => {
-//         clientController.getRandomClient().then(client => {
-//             let id = client._id;
-//             supertest(server).delete(`/api/client/${id}`)
-//                 .expect('Content-Type', /json/)
-//                 .expect(200)
-//                 .end((err, res) => {
-//                     if (err)
-//                         return done(err);
-//                     done();
-//                 });
-//         }).catch(err => {
-//             done(err);
-//         });
-//     });
-// });
+describe('API Client', function () {
+    it('POST should create an client', done => {
+        let client = {
+            name: 'Gerardo',
+            lastnamefa: 'Gutierrez',
+            lastnamemo: 'Castaneda',
+            birthdate: new Date(1993, 11, 20),
+            address: 'Somewhere #12'
+        };
+        supertest(server).post('/api/client')
+            .set('Accept', 'application/json')
+            .send(client)
+            .expect(200)
+            .then(res => done())
+            .catch(err => done(err));
+    });
+    it('Get should fetch all clients', done => {
+        supertest(server).get('/api/client')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(res => {
+                id = res.body._id;
+                done();
+            })
+            .catch(err => { done(err); });
+    });
+    it('Should get client /id', done => {
+        clientController.getRandomClient().then(client => {
+            let id = client._id;
+            supertest(server).get(`/api/client/${id}`)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                    if (err)
+                        return done(err);
+                    done();
+                });
+        }).catch(err => {
+            done(err);
+        });
+    });
+    it('Should delete client /id', done => {
+        clientController.getRandomClient().then(client => {
+            let id = client._id;
+            supertest(server).delete(`/api/client/${id}`)
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                    if (err)
+                        return done(err);
+                    done();
+                });
+        }).catch(err => {
+            done(err);
+        });
+    });
+});
 
 describe('Types client', () => {
     it('Check the element post have the correct type', done => {
